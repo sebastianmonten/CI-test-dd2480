4e02a21acbad849de4f1bc21259af5ce85c435f2
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2024-02-07 15:40:34 +0100

    feat: add error handling for the json parsing, change log file writing routing

--- server.py
+++ server.py
@@ -1,21 +1,48 @@
+import os
 from flask import Flask, request
+import datetime
 
 app = Flask(__name__)
 
 def extract_basic_info(data):
-    repo_name = data.get('repository', {}).get('name', 'Unknown Repo')
-    pusher_name = data.get('pusher', {}).get('name', 'Unknown Pusher')
-    branch_ref = data.get('ref', 'Unknown Branch')
-    return f"Repo: {repo_name}, Pusher: {pusher_name}, Branch: {branch_ref}"
+    try:
+        repo_name = data['repository']['name']
+        pusher_name = data['pusher']['name']
+        branch_ref = data['ref']
+        commit_id = data['after']
+
+
+        timestamp_str = data['head_commit']['timestamp']
+        timestamp = datetime.datetime.fromisoformat(timestamp_str)
+        date = timestamp.date()
+        time = timestamp.time()
+        
+    except KeyError:
+        return "Error: Requred data missing from json file!"
+
+    
+    return f"Repo: {repo_name}, Pusher: {pusher_name}, Branch: {branch_ref}, Timestamp: {date} {time}, Commit ID: {commit_id}"
 
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     data = request.json
     basic_info = extract_basic_info(data)
-    # Write basic information of the received JSON payload to log.txt
-    with open('/tmp/log.txt', 'a') as f:
+    # Get the directory of the current script and use it to create the log file path
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    log_file_path = os.path.join(script_dir, 'tmp', 'log.txt')
+    with open(log_file_path, 'a') as f:
         f.write(f"Received webhook payload: {basic_info}\n")
     return 'Webhook received successfully', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8027)
+
+
+
+'''
+timestamp 
+ref
+user
+commit id (last)
+
+'''
